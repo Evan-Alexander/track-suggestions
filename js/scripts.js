b8c41e6f29911627043cfbb9b238084d17dcdd97
Jason Brown <jasontbrown99@gmail.com>
2017-01-13 15:45:30 -0800

    switched name and #blank around, replaced class addressed with class name in #show. JS will now work for first section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
   $("#blank form").submit(function(event) {
     var nameInput = $("input#name").val();
-    // var size = $("select#size").val();
-    //
-    // if {
-    //   (size )
-    // }
 
     $(".name").text(nameInput);
-
 
     $("#show").show();
 
