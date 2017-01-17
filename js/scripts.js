4eb69bd609ffaef93bd8aafd471f4b03585b9ca2
Jason Brown <jasontbrown99@gmail.com>
2017-01-16 17:46:04 -0800

    Fix easy section to work with js.

--- js/scripts.js
+++ js/scripts.js
@@ -32,47 +32,47 @@
 
 
 $(document).ready(function() {
-  $("#blank form").submit(function(event) {
-    var nameInput = $("input#name").val();
-
-    $(".name").text(nameInput);
-    $("#show").show();
-
-    event.preventDefault();
-  });
-
-  $("#blank form").submit(function(event) {
-    var companySize = $("#size").val();
-    if (companySize === "small") {
-      $("div#ruby").show();
-    } else if (companySize === "medium") {
-      $("div#php").show();
-    } else if (companySize === "large") {
-      $("div#java").show();
-    }
-
-    event.preventDefault();
-  });
-
-  $("#blank form").submit(function(event) {
-    var demand = $("#demand").val();
-    if (demand === "high") {
-      $("div#php").show();
-      $("div#java").show();
-    } else if (companySize === "no-matter") {
-      $("div#php").show();
-      $("div#java").show();
-      $("div#ruby").show();
-    }
-
-    event.preventDefault();
-  });
+  // $("#blank form").submit(function(event) {
+  //   var nameInput = $("input#name").val();
+  //
+  //   $(".name").text(nameInput);
+  //   $("#show").show();
+  //
+  //   event.preventDefault();
+  // });
+  //
+  // $("#blank form").submit(function(event) {
+  //   var companySize = $("#size").val();
+  //   if (companySize === "small") {
+  //     $("div#ruby").show();
+  //   } else if (companySize === "medium") {
+  //     $("div#php").show();
+  //   } else if (companySize === "large") {
+  //     $("div#java").show();
+  //   }
+  //
+  //   event.preventDefault();
+  // });
+  //
+  // $("#blank form").submit(function(event) {
+  //   var demand = $("#demand").val();
+  //   if (demand === "high") {
+  //     $("div#php").show();
+  //     $("div#java").show();
+  //   } else if (companySize === "no-matter") {
+  //     $("div#php").show();
+  //     $("div#java").show();
+  //     $("div#ruby").show();
+  //   }
+  //
+  //   event.preventDefault();
+  // });
 
   $("#blank form").submit(function(event) {
     var simple = $("#simple").val();
     if (simple === "easy") {
       $("div#ruby").show();
-    } else if (companySize === "whatever") {
+    } else if (simple === "whatever") {
       $("div#php").show();
       $("div#java").show();
     }
